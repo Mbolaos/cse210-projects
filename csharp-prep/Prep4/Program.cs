78f63cbebc001050cbe9f87613de4ace7f8ca7d2
Michael Bolanos <michaelbolaos1@gmail.com>
2023-01-12 14:53:41 -0600

    Finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished ");
+
+        List<int> numbers = new List<int>();
+
+         int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number: ");            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        {
+        int sum = 0;
+            foreach (int number in numbers)
+           {
+                sum += number;
+           }
+          Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+          Console.WriteLine($"The average is: {average}");
+        }
+         int maxNumber = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                // if this number is greater than the max, we have found the new max!
+                maxNumber = number;
+            }
+        }
+        Console.WriteLine($"The max is: {maxNumber}");
     }
 }

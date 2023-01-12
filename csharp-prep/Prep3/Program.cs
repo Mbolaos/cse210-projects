290217dc45fa4af9bab0963eb7e25aa6f7418427
Michael Bolanos <michaelbolaos1@gmail.com>
2023-01-12 13:29:33 -0600

    Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicnumber = int.Parse(Console.ReadLine());
+
+        int guess = -1;
+          while (guess != magicnumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicnumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicnumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            //I tried to create a loop to let the user the chance to answer yes to continue playing but dont had succes can we get some tips on how to do that?
+
+        }
     }
 }
